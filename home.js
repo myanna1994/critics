be3a3d32ab2e9e37b51fa8d9cac888b567d14849
Anna Lin <sakurax7@icloud.com>
2017-01-29 22:11:47 +0800

    renew

--- home.js
+++ home.js
@@ -2,30 +2,25 @@ var request = require('request');
 request('https://api.douban.com/v2/movie/in_theaters', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var newbody = JSON.parse(body)
-
-     // Show the HTML for the Google homepage.
     for( var i = 0; i <= newbody.total; i++){
-
-
-    var OriginalTitle = newbody.subjects[0].original_title;
-    if (newbody.subjects[0].original_title = newbody.subjects[0].title){
-      OriginalTitle = " ";
-    } else{
-      OriginalTitle = newbody.subjects[0].original_title;
-    }
-    console.log("片名：" + newbody.subjects[0].title + OriginalTitle);
-    var id = newbody.subjects[0].id;
-    var website = "https://api.douban.com/v2/movie/subject/" + id;
-    var alt = newbody.subjects[0].alt;
-
-    var request = require('request');
-    request(website, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var renewbody = JSON.parse(body);
-        var directors = renewbody.directors[0].name;
-        console.log("导演：" + directors);
-        var rating0 = renewbody.rating.average;
-        console.log("评分：" + rating0)
+      var OriginalTitle = newbody.subjects[0].original_title;
+      if (newbody.subjects[0].original_title = newbody.subjects[0].title){
+        OriginalTitle = " ";
+      } else{
+        OriginalTitle = newbody.subjects[0].original_title;
+      }
+      console.log("片名：" + newbody.subjects[0].title + OriginalTitle);
+       var id = newbody.subjects[0].id;
+       var website = "https://api.douban.com/v2/movie/subject/" + id;
+       var alt = newbody.subjects[0].alt;
+       var request = require('request');
+       request(website, function (error, response, body) {
+       if (!error && response.statusCode == 200) {
+         var renewbody = JSON.parse(body);
+       var directors = renewbody.directors[0].name;
+       console.log("导演：" + directors);
+       var rating0 = renewbody.rating.average;
+       console.log("评分：" + rating0)
     }})}
 }
 })
