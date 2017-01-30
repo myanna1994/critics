dbce74830a24c88c4526f71916b49ba3ce2b82e8
Anna Lin <sakurax7@icloud.com>
2017-01-30 20:47:23 +0800

    evening

--- test.js
+++ test.js
@@ -1,11 +1,22 @@
 var request = require('request');
 request('https://api.douban.com/v2/movie/in_theaters', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    var newbody = JSON.parse(body)
-
-    for(var i = 0; i < newbody.count ; i++){
-      var id = newbody.subjects[i].id;
-      console.log(id);
-      var website = "https://api.douban.com/v2/movie/subject/" + id;
+    var newbody = JSON.parse(body);
+    //console.log(newbody);
+    for(var a = 0 ; a < newbody.total; a++  ){
+      var t = 0;
+      var x = 0;
+      for( var i = 0 + a; i< newbody.total ; i++ ){
+        var rating = newbody.subjects[i].rating.average;
+        if (rating > t){
+          t = rating;
+          x = i;
+        }
+      }
+      var temp = newbody.subjects[a]
+      newbody.subjects[a] = newbody.subjects[x];
+      newbody.subjects[x] = temp;
+    }
+    console.log(newbody)
   }
-}})
+})
